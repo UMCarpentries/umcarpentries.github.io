ebfd49cd1598a8291d82aafb00578207d0f7c5f0
Kelly Sovacool <kellysovacool@gmail.com>
2019-11-19 16:10:24 -0500

    Handle new workshop template with 'carpentry' in _config.yaml and swc syllabus dir in 'swc'

--- update-workshops.py
+++ update-workshops.py
@@ -42,6 +42,17 @@ def main(args):
     else:
         write_all_posts(repos, args['--workdir'], dryrun=args['--dryrun'])
 
+def test():
+    """ Get repos for testing interactively
+    :return: list of pygithub repos
+    """
+    with open(".token", 'r') as token_file:
+        token = token_file.readline().strip()
+    github = Github(token)
+    user_swc = github.get_user('umswc')
+    return sorted([repo for repo in user_swc.get_repos() if repo.name.split('-')[0].isnumeric()],
+                   key=lambda repo: repo.name, reverse=True)
+
 
 def remove_old_posts(workdir, dryrun=False):
     """ Remove past workshops from the posts directory
@@ -157,9 +168,15 @@ class Workshop:
         """
         repo_contents = [file for file in repo.get_contents("", ref="gh-pages") if file.path in {"index.md",
                                                                                              "index.html"}]  #
-        # index could be markdown or html
+        # index could be markdown or html. expect only one
         index_file = repo_contents.pop()
         header = {key: (value if value else '') for key, value in yaml.load( base64.b64decode(index_file.content).decode('utf-8').strip("'").split('---')[1], Loader=yaml.Loader).items()}
+        if 'carpentry' not in header: # new template has 'carpentry' field in _config.yaml only, not index.md
+            config_file = repo.get_contents("_config.yml", ref="gh-pages")
+            config_dict = yaml.load(base64.b64decode(config_file.content).decode('utf-8'), Loader=yaml.Loader)
+            if 'carpentry' in config_dict:
+                header['carpentry'] = config_dict['carpentry']
+
         return header
 
     @classmethod
@@ -170,11 +187,19 @@ class Workshop:
         :return: string containing the lesson titles separated by commas
         """
         material = list()
-        filepath = f"_includes/{'sc' if carpentry == 'swc' else carpentry}/syllabus.html"
+        includes_dir = "_includes"
+        content_paths = {file.path for file in repo.get_contents(includes_dir, ref="gh-pages")}
         # TO-DO: handle old-style workshop repos with syllabus content in index.html
         # (e.g. https://github.com/UMSWC/2017-12-18-umich)
-        content_paths = {file.path for file in repo.get_contents("_includes", ref="gh-pages")}
-        if '_includes/sc' in content_paths:
+        if f"{includes_dir}/{carpentry}" in content_paths:
+            filepath = f"{includes_dir}/{carpentry}/syllabus.html"
+        elif carpentry == "swc" and f"{includes_dir}/sc" in content_paths:
+            # previous abbr for 'swc' dir was 'sc'; need to handle both
+            filepath = f"{includes_dir}/sc/syllabus.html"
+        else:
+            filepath = None
+        # only get syllabus if the file could be found
+        if filepath:
             syllabus = [line.strip() for line in base64.b64decode(repo.get_contents(filepath,
                                                                                      ref="gh-pages").content).decode(
                 'utf-8').strip("'").split('\n')]
@@ -214,7 +239,6 @@ def join_list(this_list):
     :return: a string of the list items separated by hyphens & newlines.
     """
     return '\n'.join(f"- {thing}" for thing in this_list)
-
 
 if __name__ == "__main__":
     main(docopt(__doc__))
